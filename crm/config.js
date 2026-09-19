274fcc6233be757a42ae4f1df0040793610f0304
redrockjohn12 <redrockjohn12@users.noreply.github.com>
2026-09-19 08:19:33 -0100

    Connect Atlas CRM to Supabase

---
+++ crm/config.js
@@ -0,0 +1,6 @@
+window.ATLAS_SUPABASE_CONFIG = {
+  url: "https://cogamcqicefmyedjfvnc.supabase.co",
+  key: "sb_publishable_JXgkKk5GHMtGQgQFmMO_3Q_NUQQ0qYg",
+  siteName: "Atlas Technologies",
+  siteUrl: "https://redrockjohn12.github.io/AtlasTechnologies/"
+};
